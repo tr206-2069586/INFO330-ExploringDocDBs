656ccd183fb5f741e2995252225edfcfb0285436
merp206 <tyler.merp@Tylers-MacBook-Air-4.local>
2023-05-05 20:34:53 -0700

    fin

--- Battle.py
+++ Battle.py
@@ -8,19 +8,24 @@ pokemonColl = pokemonDB['pokemon_data']
 def fetch(pokemonid):
     return pokemonColl.find_one({"pokedex_number":pokemonid})
 
+# Modified battle outcome so now it's a weighted outcome based on who has more advantages
 def battle(pokemon1, pokemon2):
     print("Let the Pokemon battle begin! ================")
     print("It's " + pokemon1['name'] + " vs " + pokemon2['name'])
-
+    p1 = 0
+    p2 = 0
     for stat in ['hp', 'attack', 'defense', 'speed', 'sp_attack', 'sp_defense']:
         if pokemon1[stat] > pokemon2[stat]:
+            p1 += 1
             print(pokemon1['name'] + " has the advantage in " + stat)
         elif pokemon2[stat] > pokemon1[stat]:
+            p2 += 1
             print(pokemon2['name'] + "'s " + stat + " is superior")
-
-    winner = random.randrange(2)
-    if winner == 0: print("Battle results: " + pokemon1['name'])
-    if winner == 1: print("Battle results: " + pokemon2['name'])
+    p1WinChange = (float(p1/6) * 100)
+    p2WinChange = (float(p2/6) * 100)
+    winner = random.choices([0,1], weights=(p1WinChange,p2WinChange), k=1)
+    if winner[0] == 0: print("Battle results: " + pokemon1['name'])
+    if winner[0] == 1: print("Battle results: " + pokemon2['name'])
 
 def main():
     # Fetch two pokemon from the MongoDB database
